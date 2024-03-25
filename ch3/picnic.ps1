c1d0178502fe3ee3888206c2a5b178f100e7485d
Mocha <126608230+MochaTheBear@users.noreply.github.com>
2024-03-25 18:34:01 -0400

    initial working submission for picnic problem

---
+++ ch3/picnic.ps1
@@ -0,0 +1,33 @@
+<#
+
+Author:            Jonathan Pizzano
+Email:             jpizzanotech@outlook.com
+
+Blog:              Soon!
+GitHub:            https://github.com/MochaTheBear
+
+LinkedIn:          https://www.linkedin.com/in/jonathan-pwsh/
+
+#>
+param(
+    [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)]
+    [string[]] $items,
+    [switch] $sorted
+)
+$numberOfItems = $items.Length
+$templateString = New-Object -TypeName 'System.Text.StringBuilder' -ArgumentList "You are bringing "
+$finalPlace = $numberOfItems - 1
+
+# $numberOfItems cannot be less than 1 because of the Mandatory parameter
+Switch ($numberOfItems){
+    1 { $templateString.Append("{0}.") }
+    2 { $templateString.Append("{0} and {1}.") }
+    Default { for ($i = 0; $i -lt $finalPlace; $i++ ) { $templateString.Append("{$i}, ") }
+              $templateString.Append("and {$finalPlace}.") }
+}
+
+if ($sorted) {
+    $items = $items | Sort-Object
+}
+
+$templateString.ToString() -f $items
