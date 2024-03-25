93d8487a46fde639b14050ff3ce1299407074ac2
Mocha <126608230+MochaTheBear@users.noreply.github.com>
2024-03-24 22:58:03 -0400

    Added contact info

--- ch1/Hello.ps1
+++ ch1/Hello.ps1
@@ -1,3 +1,15 @@
+<#
+
+Author:            Jonathan Pizzano
+Email:             jpizzanotech@outlook.com
+
+Blog:              Soon!
+GitHub:            https://github.com/MochaTheBear
+
+LinkedIn:          https://www.linkedin.com/in/jonathan-pwsh/
+
+#>
+
 $userInput = Read-Host "Who would you like to greet? `n"
 
 if ([string]::IsNullOrWhiteSpace($userInput))
