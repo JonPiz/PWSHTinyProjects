a1c4f238c77e60466c91c3f42e19f9afc4d930f7
Mocha <126608230+MochaTheBear@users.noreply.github.com>
2024-03-26 18:58:34 -0400

    added author info and started on ch5

--- ch4/jump.ps1
+++ ch4/jump.ps1
@@ -1,3 +1,14 @@
+<#
+
+Author:            Jonathan Pizzano
+Email:             jpizzanotech@outlook.com
+
+Blog:              Soon!
+GitHub:            https://github.com/MochaTheBear
+
+LinkedIn:          https://www.linkedin.com/in/jonathan-pwsh/
+
+#>
 param(
     [Parameter(Mandatory=$true)]
     [string]$inputText
